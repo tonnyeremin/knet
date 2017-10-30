6b6a26c7334d7092a441d7e5732dfb943a84e744
Anton <an.eremin@gmail.com>
2017-10-30 16:36:19 +0100

    Clean up + gtignore

--- Knet/BUILD.ps1
+++ Knet/BUILD.ps1
@@ -2,3 +2,6 @@
 Write-Host 'Dowloading Kubernetes api description from: https://github.com/kubernetes/kubernetes'
 (New-Object Net.WebClient).DownloadFile('https://github.com/kubernetes/kubernetes/blob/master/api/openapi-spec/swagger.json', 'swagger.json')
 
+
+
+
